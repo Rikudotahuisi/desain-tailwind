a03f4a7a19065ce2580840ef1c8160679cba9631
Marvin <email_anda@gmail.com>
2026-09-17 10:54:56 +0700

    update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -118,7 +118,7 @@
               isHomePage && !isScrolled ? 'text-white/70 hover:bg-white/10 hover:text-white' : 'text-slate-600 hover:bg-slate-100 hover:text-slate-900'
             ]"
           >
-            Medical Check Up
+            Layanan
           </router-link>
 
           <router-link to="/articles" class="rounded-full px-4 py-2 text-[13px] font-medium transition-all duration-200"
